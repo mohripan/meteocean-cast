e65f03dd6eeddce4b9c4b9fdbfaa5445e7fd1200
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2025-11-09 17:19:00 +0700

    Move Meteocean.Services inside Services folder

---
+++ services/meteocean-cast.services/src/Meteocean.Services/Units.cs
@@ -0,0 +1,11 @@
+﻿namespace Meteocean.Services
+{
+    /// <summary>Canonical units used in normalized series.</summary>
+    public static class Units
+    {
+        public const string Meters = "m";
+        public const string Seconds = "s";
+        public const string DegreesFrom = "deg_from";
+        public const string MetersPerSecond = "m/s";
+    }
+}
